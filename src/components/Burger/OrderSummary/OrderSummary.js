efd4519abae49c631c14e2d23db0113cbb68ef5f
croaton <dmytro.shervarly@gmail.com>
2020-05-07 22:12:25 +0200

    First version of a bugrer building app

---
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react';
+
+import Button from '../../UI/Button/Button';
+
+class OrderSummary extends Component {
+    componentWillUpdate() {
+        console.log('Order summary will update');
+    }
+
+    render() {
+        const ingredientSummary = Object.keys(this.props.ingredients)
+        .map(igKey => {
+        return (
+            <li key={igKey}>
+                <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {this.props.ingredients[igKey]}
+            </li>
+        )});
+    
+        return (
+        <React.Fragment>
+            <h3>Your order</h3>
+            <p>A delicious burger with the following ingredients:</p>
+            <ul>
+                {ingredientSummary}
+            </ul>
+            <p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
+            <p>Continue to checkout?</p>
+            <Button btnType="Danger" clicked={this.props.purchaseCanceled}>CANCEL</Button>
+            <Button btnType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
+        </React.Fragment>
+        )
+    }
+};
+
+
+export default OrderSummary;
